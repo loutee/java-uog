e302d330f898ad0cfbd6d2176586479790e0b488
Louis Tran <loutran987@gmail.com>
2016-12-25 14:51:42 -0800

    Added MyFirstJavaProgram consisting from Basic Syntax lecture material

---
+++ java-ref/MyFirstJavaProgram.java
@@ -0,0 +1,18 @@
+// MyFirstJavaProgram.java
+// The name of the program file should match the class name exactly!
+
+// The first letter of any class name should be in uppercase.
+public class MyFirstJavaProgram {
+	/*	
+ 	This is my first java program.
+	This will print 'Hello World' as the output
+*/
+	
+	// The first letter of any method name should be in lowercase.
+	// Java program processing starts from the main() method, which is
+	// mandatory for every Java program.
+	public static void main(String []args) {
+		System.out.println("Hello World");
+		// Prints Hello World
+	}
+}
